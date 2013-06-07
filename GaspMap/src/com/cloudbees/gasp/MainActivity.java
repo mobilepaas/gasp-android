6341ad2b5ca9de52e72f63d93a9b353b640f821f
mqprichard <mqprichard@gmail>
2013-06-06 23:19:06 -0700

    Updated and tested with 4.2.2

--- GaspMap/src/com/cloudbees/gasp/MainActivity.java
+++ GaspMap/src/com/cloudbees/gasp/MainActivity.java
@@ -20,7 +20,6 @@ import com.cloudbees.gasp.model.GeoLocation;
 import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -33,11 +32,11 @@ import android.location.Address;
 import android.location.Criteria;
 import android.location.Geocoder;
 import android.location.Location;
-//import android.location.LocationListener;
+import android.support.v4.app.FragmentActivity;
+import com.google.android.gms.maps.*;
 import android.location.LocationManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.Menu;
 
@@ -45,8 +44,7 @@ public class MainActivity extends FragmentActivity {
     private static final String TAG = MainActivity.class.getName();
 
     private class LongRunningGetIO extends AsyncTask<Void, Void, String> {
-        protected String getASCIIContentFromEntity(HttpEntity entity)
-                throws IllegalStateException, IOException {
+        protected String getASCIIContentFromEntity(HttpEntity entity) throws IllegalStateException, IOException {
             InputStream in = entity.getContent();
             StringBuffer out = new StringBuffer();
             int n = 1;
@@ -62,20 +60,19 @@ public class MainActivity extends FragmentActivity {
         protected String doInBackground(Void... params) {
             HttpClient httpClient = new DefaultHttpClient();
             HttpContext localContext = new BasicHttpContext();
-            HttpGet httpGet = new HttpGet(
-                    "http://gasp-mongo.mqprichard.cloudbees.net/locations/get");
+            HttpGet httpGet = new HttpGet("http://gasp-mongo.mqprichard.cloudbees.net/locations/get");
             String text = null;
             try {
-                HttpResponse response = httpClient.execute(httpGet,
-                        localContext);
+                HttpResponse response = httpClient.execute(httpGet, localContext);
                 HttpEntity entity = response.getEntity();
                 text = getASCIIContentFromEntity(entity);
                 Log.i(TAG, text);
                 Gson gson = new Gson();
-                Type type = new TypeToken<List<GeoLocation>>(){}.getType();
+                Type type = new TypeToken<List<GeoLocation>>() {
+                }.getType();
                 List<GeoLocation> list = gson.fromJson(text, type);
                 Iterator<GeoLocation> iterator = list.iterator();
-                while(iterator.hasNext()){
+                while (iterator.hasNext()) {
                     GeoLocation geoLocation = iterator.next();
                     Log.i(TAG, geoLocation.getName());
                     Log.i(TAG, " " + geoLocation.getFormattedAddress());
@@ -95,11 +92,9 @@ public class MainActivity extends FragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        GooglePlayServicesUtil
-                .isGooglePlayServicesAvailable(getApplicationContext());
+        GooglePlayServicesUtil.isGooglePlayServicesAvailable(getApplicationContext());
 
-        GoogleMap map = ((SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map)).getMap();
+        GoogleMap map = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
 
         LocationManager locationManager;
         String svcName = Context.LOCATION_SERVICE;
@@ -126,8 +121,7 @@ public class MainActivity extends FragmentActivity {
             if (!Geocoder.isPresent()) Log.i(TAG, "No geocoder available");
             else {
                 try {
-                    List<Address> addresses = gc.getFromLocation(latitude,
-                            longitude, 1);
+                    List<Address> addresses = gc.getFromLocation(latitude, longitude, 1);
                     StringBuilder sb = new StringBuilder();
                     if (addresses.size() > 0) {
                         Address address = addresses.get(0);
@@ -152,13 +146,12 @@ public class MainActivity extends FragmentActivity {
         // new LatLng(location.getLatitude(),
         // location.getLongitude())).title("My Location"));
 
-        LatLng myLocation = new LatLng(location.getLatitude(),
-                location.getLongitude());
+        LatLng myLocation = new LatLng(location.getLatitude(), location.getLongitude());
         // map.addMarker(new
         // MarkerOptions().position(myLocation).title("My Location"));
 
-        CameraPosition cameraPosition = new CameraPosition.Builder()
-                .target(myLocation).zoom(16).bearing(0).tilt(0).build();
+        CameraPosition cameraPosition = new CameraPosition.Builder().target(myLocation).zoom(16).bearing(0).tilt(0)
+                .build();
         map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
 
         // Use gasp-mongo REST service
@@ -186,14 +179,11 @@ public class MainActivity extends FragmentActivity {
                 addresses = gc.getFromLocationName(streetAddress, 1);
 
                 if (addresses.size() > 0) {
-                    Log.i(TAG, restaurantName + ": "
-                            + addresses.get(0).toString());
+                    Log.i(TAG, restaurantName + ": " + addresses.get(0).toString());
 
                     // Add marker to map
-                    LatLng pos = new LatLng(addresses.get(0).getLatitude(),
-                            addresses.get(0).getLongitude());
-                    map.addMarker(new MarkerOptions().position(pos).title(
-                            restaurantName));
+                    LatLng pos = new LatLng(addresses.get(0).getLatitude(), addresses.get(0).getLongitude());
+                    map.addMarker(new MarkerOptions().position(pos).title(restaurantName));
                 }
             }
         }
